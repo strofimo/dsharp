4359e164bc3a2c92482fa9de62a59f633b2a88d8
MGSOPS\fredp <fred.perkins@derivco.co.uk>
2018-06-08 10:17:44 +0100

    - Migrated scripts build to use node js instead of a locally built executable - Started migrated and removal of non CLR compliant types and code

---
+++ src/DSharp.Script/src/System/Interfaces.js
@@ -0,0 +1,11 @@
+// Contracts
+
+function IObserver() { }
+function IApplication() { }
+function IContainer() { }
+function IObjectFactory() { }
+function IEventManager() { }
+function IInitializable() { }
+function IDisposable() { }
+function IEnumerator() { }
+function IEnumerable() { }
