6229c9a5c46d7c50fc6f0b6513b5daa69e893a9e
Ivan Sanz Carasa <ivansanzcarasa@gmail.com>
2019-07-15 09:08:30 +0200

    remove warnings on attribute (#127)

--- src/DSharp.Mscorlib/Attribute.cs
+++ src/DSharp.Mscorlib/Attribute.cs
@@ -5,6 +5,7 @@ namespace System
 {
     [EditorBrowsable(EditorBrowsableState.Never)]
     [ScriptIgnore]
+    [TypeForwardedFrom("mscorlib, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089")]
     public abstract class Attribute
     {
     }
