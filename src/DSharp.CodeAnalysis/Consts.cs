063e56cc88df977103afa5257617373eff4b2b8d
MGSOPS\fredp <fred.perkins@derivco.co.uk>
2019-11-06 14:33:36 +0000

    Adding CodeAnalysis projects ready to be bundled as part of DSharp

---
+++ src/DSharp.CodeAnalysis/Consts.cs
@@ -0,0 +1,10 @@
+﻿namespace DSharp.CodeAnalysis
+{
+    public static class Consts
+    {
+        public const string GenericTypeArgumentsMissingId = "DSE001";
+        public const string ScriptIgnoreGenericArgumentsAttributeMissingId = "DSE002";
+
+        public const string Category = "Generics";
+    }
+}
---
+++ src/DSharp.CodeAnalysis/MethodSymbolExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using Microsoft.CodeAnalysis;
+
+namespace DSharp.CodeAnalysis
+{
+    public static class MethodSymbolExtensions
+    {
+        private const string ScriptIgnoreGenericArgumentsAttribute = "ScriptIgnoreGenericArguments";
+
+        public static bool HasScriptIgnoreGenericArgumentsAttribute(this IMethodSymbol methodSymbol)
+        {
+            return methodSymbol
+                .GetAttributes()
+                .Any(a => a.AttributeClass.Name.StartsWith(ScriptIgnoreGenericArgumentsAttribute));
+        }
+    }
+}
