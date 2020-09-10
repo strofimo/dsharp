3b6b187facba89356c055a92541c476cf1779a9c
IvanSa <ivan.sanzcarasa@derivco.co.uk>
2020-09-10 10:52:54 +0200

    remove unused 'using'

--- src/DSharp.Compiler/Generator/ScriptMetadataGenerator.cs
+++ src/DSharp.Compiler/Generator/ScriptMetadataGenerator.cs
@@ -8,7 +8,6 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Text;
 using DSharp.Compiler.ScriptModel.Symbols;
 
 namespace DSharp.Compiler.Generator
